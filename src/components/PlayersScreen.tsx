3cf3f4cfd8613fcdef89e1cd49c6fc5be2701fda
Tim <timwagner240@gmail.com>
2026-09-14 15:58:15 -0500

    Made more universal condition for checking numPlayers
    Added comment for future planning

--- src/components/PlayersScreen.tsx
+++ src/components/PlayersScreen.tsx
@@ -11,9 +11,9 @@ export default function PlayersScreen() {
   const [addEnabled, setAddEnabled] = useState<boolean>(true);
 
   useEffect(() => {
-    if (numPlayers == MAX_PLAYERS) {
+    if (numPlayers >= MAX_PLAYERS) {
       setAddEnabled(false);
-    } else if (numPlayers == MIN_PLAYERS) {
+    } else if (numPlayers <= MIN_PLAYERS) {
       setRemoveEnabled(false);
     } else {
       setAddEnabled(true);
@@ -29,7 +29,13 @@ export default function PlayersScreen() {
     setNumPlayers(numPlayers - 1);
   }
 
-  // TODO: need to pass state from here to the Button components to properly style when add or remove isn't able to be clicked
+  // TODO: add a check for the contents of each input area to verify that each player name has been filled in
+  // Right now, it's probably easiest to manually check each PlayerInput and if they all have content, load them into the players,
+  // which is state that should be passed from index.tsx when rendering the component. Then, it can start to handle the backend logic
+  // of either using an existing player or creating a new player.
+  // The goal is ultimately to avoid having a "save" button that must be pressed, instead opting to have it automatically detect
+  // that all inputs are valid before enabling the Start Game button.
+
   return (
     <View style={styles.playersScreenView}>
       {Array.from({ length: numPlayers }, (_, index) => {
